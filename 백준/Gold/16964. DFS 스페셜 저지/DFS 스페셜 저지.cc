2ea19c28950ce5e0a76b8ca2c75c4e923db3e11e
dhee-jung <aniketoss@naver.com>
2023-10-18 03:57:31 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 144 ms, Memory: 20980 KB -BaekjoonHub

--- 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.cc
+++ 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.cc
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-#define _FR(...) // freopen(__VA_ARGS__)
+#define _FR(...)  // freopen(__VA_ARGS__)
 
 #define fi first
 #define se second
@@ -14,16 +14,10 @@ using namespace std;
 
 #define MAXN (111111)
 
-struct Node {
-  int x;
-  int next;
-} nodes[MAXN];
-
 int N;
 int cnt;
 bool flag;
 vector<bool> visit(MAXN);
-vector<int> gp[MAXN];
 vector<int> ord(MAXN);
 unordered_map<int, set<int>> tree;
 void init() {
@@ -31,15 +25,8 @@ void init() {
   visit.clear();
   flag = false;
   for (int i = 0; i < N - 1; i++) {
-    gp[i].clear();
-  }
-  for (int i = 0; i < N - 1; i++) {
     int u, v;
     scanf("%d %d", &u, &v);
-    gp[v].pb(u);
-    gp[u].pb(v);
-    nodes[v] = {v, u};
-    nodes[u] = {u, v};
     tree[u].insert(v);
     tree[v].insert(u);
   }
@@ -56,7 +43,7 @@ void dfs(int x, int depth) {
   }
   visit[x] = true;
   cnt++;
-  for (int i = 0; i < gp[x].size(); i++) {
+  for (int i = 0; i < tree[x].size(); i++) {
     if (!visit[ord[cnt]] && tree[x].find(ord[cnt]) != tree[x].end()) {
       dfs(ord[cnt], depth + 1);
     }
