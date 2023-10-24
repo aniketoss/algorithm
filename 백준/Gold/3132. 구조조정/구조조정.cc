f889d02f81fabf64bbbacda97396ecd2bb0492d6
dhee-jung <aniketoss@naver.com>
2023-10-25 00:03:18 +0900

    Update 구조조정.cc

--- 백준/Gold/3132. 구조조정/구조조정.cc
+++ 백준/Gold/3132. 구조조정/구조조정.cc
@@ -4,13 +4,18 @@
 
 using namespace std;
 
-#define MAX 1000
+#define MAXN (1111)
+
+#define pb push_back
+#define _FR(...) freopen(__VA_ARGS__)
 
 int n;
-int iq[MAX + 1];
-vector<int> adj[MAX + 1];
+int iq[MAXN];
+vector<int> adj[MAXN];
 
-bool iq_cmp(int a, int b) { return iq[a] > iq[b]; }
+bool cmp4iq(const int& a, const int& b) {
+    return iq[a] > iq[b];
+}
 
 int main(void) {
   scanf("%d", &n);
@@ -19,12 +24,12 @@ int main(void) {
 
   for (int i = 1; i < n; ++i) {
     int a, b;
-    scanf("%d%d", &a, &b);
-    adj[a].push_back(b);
+    scanf("%d %d", &a, &b);
+    adj[a].pb(b);
   }
 
   for (int i = 1; i <= n; ++i) {
-    sort(adj[i].begin(), adj[i].end(), iq_cmp);
+    sort(adj[i].begin(), adj[i].end(), cmp4iq);
 
     for (int j = 1; j < adj[i].size(); ++j)
       printf("%d %d\n", adj[i][j - 1], adj[i][j]);
